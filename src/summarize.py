32c33e9f8e20e73abe3f68fa389370bfa4c992b0
David Miller <108412723+DavidMiller132@users.noreply.github.com>
2023-03-16 14:24:30 -0400

    Update summarize.py

--- src/summarize.py
+++ src/summarize.py
@@ -103,7 +103,7 @@ def query_summary(query: str, messages: Dict[int, str]) -> str:
 
 	return completion.choices[0].text.strip()
 
-start_sequence = "Summarize text channel activity in one sentence, including who was speaking, what they were speaking about, and for how long they were speaking about it. Then provide chat highlights in the form of 5 bullet points, in your own words. "
+start_sequence = "Please, send back corrected messages that may have had any grammatical issues"
 
 restart_sequence = "Are there any more questions you have?"
 
